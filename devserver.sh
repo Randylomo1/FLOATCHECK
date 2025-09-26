e59c51d3ff87b4cf0b5923c766ede24e5939a25c
foodkevs <foodkevs@gmail.com>
2025-09-26 20:18:26 +0000

    Fix: Correctly start server in devserver.sh

--- devserver.sh
+++ devserver.sh
@@ -5,5 +5,9 @@ if [ -f .env ]; then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 
-# Start all services
-docker-compose up
+# Bring up the database service in the background
+docker-compose up -d db
+
+# Run the web service, executing the entrypoint script
+# This ensures the db is ready and migrations run before starting the server.
+docker-compose run --service-ports web /app/entrypoint.sh
